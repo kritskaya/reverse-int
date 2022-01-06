a0b0d730d5b4305746fac598701d51098247f900
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-01-06 23:14:00 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+	let str = "";
+	let strNumber = Math.abs(n).toString();
+
+	for(let i = 0; i < strNumber.length; i++) {
+		str += strNumber[strNumber.length - 1 - i];
+	}
+	return +str;
 }
